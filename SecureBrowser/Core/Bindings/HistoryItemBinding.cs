f48a44dccb24124ed488339ea3593723a5929cf1
Dennise Catolos <63337204+dentolos19@users.noreply.github.com>
2021-04-25 13:20:25 +0800

    Updated history binding

--- SecureBrowser/Core/Bindings/HistoryItemBinding.cs
+++ SecureBrowser/Core/Bindings/HistoryItemBinding.cs
@@ -6,8 +6,8 @@ namespace SecureBrowser.Core.Bindings
     public class HistoryItemBinding
     {
 
-        public string? Title { get; init; }
-        public string? Address { get; init; }
+        public string Title { get; init; }
+        public string Address { get; init; }
         public DateTime Time { get; init; }
 
     }
